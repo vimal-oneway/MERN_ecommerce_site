9536adf9aaa0e0bea7e6605b1594d6baba5df6d4
Webie <90978643+vimal-oneway@users.noreply.github.com>
2023-01-05 22:12:09 +0530

    products view page && product img done

--- backend/middlewares/authenticate.js
+++ backend/middlewares/authenticate.js
@@ -4,7 +4,6 @@ const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 
 exports.isAuthenticatedUser = catchAsyncError( async(req, res, next)=>{
-    console.log("hello");
     const {token} = req.cookies;
     if(!token){
         return next(new ErrorHandler("Login first to handle this resource",401))
